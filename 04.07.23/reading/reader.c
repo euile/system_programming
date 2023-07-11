588a0436fde58234f0de1f7460a49b6702c86e8f
Алина Юзлибаева <91687337+euile@users.noreply.github.com>
2023-07-11 19:27:29 +0300

    Create reader.c

---
+++ 04.07.23/reading/reader.c
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+
+int main()
+{
+    int fd;
+    char buf[100];
+    fd = open("/dev/alina-dev",O_RDWR);
+    if (fd < 0)
+    {
+        printf("Cannot open\n");
+        return 0;
+    }
+    int readed_bytes = read(fd, buf, 5);
+    buf[5] = 0;
+    printf("Readed %d bytes\n", readed_bytes);
+    printf("%s\n", buf);
+    close(fd);
+
+    return 0;
+}
